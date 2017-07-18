fbc3cc0232c8210f087d9bafcd204ea445bba0b5
ace <andrew.emil@gmail.com>
2017-07-18 16:39:18 -0400

    Initial commit for nightbike

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float speed;
+	private Vector3 direction;
+	// Use this for initialization
+	void Start () {
+		speed = 1.0f;
+		direction = new Vector3 (1, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.position = gameObject.transform.position + speed * direction;
+	}
+}
